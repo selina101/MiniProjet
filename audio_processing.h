fe75b0d45f80e32eace30266f86181aba93e5476
selina101 <82826452+selina101@users.noreply.github.com>
2021-04-29 09:52:41 +0200

    Update audio_processing.h

--- audio_processing.h
+++ audio_processing.h
@@ -4,6 +4,14 @@
 
 #define FFT_SIZE 	1024
 
+#define FORWARDS 0
+#define LEFT 1
+#define RIGHT 2
+#define BACK 3
+#define STOP 4
+#define STUCK 5
+
+
 typedef enum {
 	//2 times FFT_SIZE because these arrays contain complex numbers (real + imaginary)
 	LEFT_CMPLX_INPUT = 0,
@@ -17,7 +25,7 @@ typedef enum {
 	BACK_OUTPUT
 } BUFFER_NAME_t;
 
-
 void processAudioData(int16_t *data, uint16_t num_samples);
+void direction_detection(float* data);
 
 #endif /* AUDIO_PROCESSING_H */
