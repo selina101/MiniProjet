108ded2f85e71d07854f6f00b61d5140724b72a8
selina101 <82826452+selina101@users.noreply.github.com>
2021-05-15 23:55:14 +0200

    Update ir_processing.h

--- ir_processing.h
+++ ir_processing.h
@@ -2,7 +2,7 @@
 #define IR_PROCESSING_H
 
 void epuck_move(int direction);
-void ok_to_move(int direction);
+void ok_to_move(int direction);//, int max_amp);
 void e_puck_follow(void);
 
 #endif /* IR_PROCESSING_H */
