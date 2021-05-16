15c61380c044970f90e7c386d5b33f35d4068ad3
selina101 <82826452+selina101@users.noreply.github.com>
2021-05-16 23:48:21 +0200

    Update fft.h

--- fft.h
+++ fft.h
@@ -1,12 +1,6 @@
 #ifndef FFT_H
 #define FFT_H
 
-
-typedef struct complex_float{
-	float real;
-	float imag;
-}complex_float;
-
 void doFFT_optimized(uint16_t size, float* complex_buffer);
 
 #endif /* FFT_H */
